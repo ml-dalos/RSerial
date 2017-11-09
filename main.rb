3c623b2addee6ea343506654cb4bbb4440013842
ml-dalos <joker.leha98@gmail.com>
2017-11-09 20:23:58 +0300

    Add features for TOKS-lab #4(Cheack collide, JAM-signal, wait busy chanel)

--- main.rb
+++ main.rb
@@ -4,11 +4,13 @@ require_relative 'port'
 ADRESS = ENV['PORT']
 BAUD_ARRAY = [110, 150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
 
-$serialport = SerialPort.new ADRESS.to_s
+serialport = SerialPort.new ADRESS.to_s
+
 Shoes.app title: 'RSerial', width: 700, height: 500 do
   background dimgray
   flow do
     @contol = stack margin: 20, width: 250 do
+    	inscription fg "Port adress: #{ADRESS}", whitesmoke
   	  caption fg 'Write your message', whitesmoke
   	  @text_box = edit_line
   	  button 'Send message' do
@@ -18,23 +20,23 @@ Shoes.app title: 'RSerial', width: 700, height: 500 do
 			 	  		para fg @text_box.text, whitesmoke 
 		  	 	  end
 	  	 	  end
-	  	 	  $serialport.write(@text_box.text)
+	  	 	  serialport.write(@text_box.text)
 	  		  @text_box.text = ''
 	  	  end
   		end
   		list_box items: BAUD_ARRAY, choose: 9600 do |list|
-        $serialport.set_baud_rate(list.text.to_i)
+        serialport.set_baud_rate(list.text.to_i)
       end 
     end
     $messages = stack(margin: 5, width: 445){ border gray } 
   end
   Thread.new do
 		loop do
-			@message = $serialport.read 250
+			@message = serialport.read
 			unless @message.empty?
 			  $messages.append do
 			 	  stack width: 200, margin_left: 250 do
-				 	  para fg @message, whitesmoke
+				 	  para fg @message, lightyellow
 			    end
 			  end
 		  end
--- port.rb
+++ port.rb
@@ -1,14 +1,70 @@
 require 'rubyserial'
 
 class SerialPort < Serial
+	
+	DELAY_SECONDS = 1
+	JAM_SIGNAL = '~'
 	attr_reader :adress
-
+	
 	def initialize adress ,baude_rate=9600, data_bits=8, parity=:none 
     @adress = adress
     super
   end
 
+  def write data
+    wait_chanel_free
+  	data.each_byte do |byte|
+      attemps = 0  
+      loop do
+     	  super byte
+  	    sleep 0.1
+  	    if collide?
+  	    	super JAM_SIGNAL 
+  	    	attemps += 1
+  	    	delay_write(attemps)
+  	    else
+  	    	break
+  	    end 
+      end
+    end
+  end
+
+  def read 
+    loop do
+    	case byte = getbyte
+      when nil
+        return message
+      when JAM_SIGNAL
+        message.chop!
+      else
+        message += byte
+      end    
+    end
+
+  end
+
   def set_baud_rate baud_rate
     this = SerialPort.new @adress, baud_rate
   end
+  
+  private
+
+  def wait_chanel_free
+  	sleep(DELAY_SECONDS) if time_odd?
+  end
+
+  def time_odd?
+  	Time.now.to_i.odd?
+  end 
+
+  def collide?
+    flag = time_odd?
+  	print 'x' if flag
+  	flag
+  end
+
+  def delay_write attemp
+  	sleep(rand([10, attemp].min ** 2) * 10)
+  end
+
 end
