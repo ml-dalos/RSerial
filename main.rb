e2bfbdd33935ceb2f5b41d1af0b26d8579e428c4
ml-dalos <joker.leha98@gmail.com>
2017-11-10 14:08:50 +0300

    Replace Tab with Spaces

--- main.rb
+++ main.rb
@@ -9,21 +9,21 @@ Shoes.app title: 'RSerial', width: 700, height: 500 do
   background dimgray
   flow do
     @contol = stack margin: 20, width: 250 do
-    	inscription fg "Port adress: #{ADRESS}", whitesmoke
-    	caption fg 'Write your message', whitesmoke
-  	  @text_box = edit_line
-  	  button 'Send message' do
-  	    unless @text_box.text.empty?
-  	      $messages.append do
-		 	    	stack width: 200 do
-		 	    		para fg @text_box.text, whitesmoke 
-		  	 	  end
-	  	 	  end
-	  	 	  serialport.write(@text_box.text)
-	  		  @text_box.text = ''
-	  	  end
-  		end
-  		list_box items: Port::BAUD_ARRAY, choose: 9600 do |list|
+      inscription fg "Port adress: #{ADRESS}", whitesmoke
+      caption fg 'Write your message', whitesmoke
+      @text_box = edit_line
+      button 'Send message' do
+        unless @text_box.text.empty?
+          $messages.append do
+            stack width: 200 do
+              para fg @text_box.text, whitesmoke 
+            end
+          end
+          serialport.write(@text_box.text)
+          @text_box.text = ''
+        end
+      end
+      list_box items: Port::BAUD_ARRAY, choose: 9600 do |list|
         serialport.set_baud_rate(list.text.to_i)
       end
       para fg "Debugger", whitesmoke
@@ -32,30 +32,30 @@ Shoes.app title: 'RSerial', width: 700, height: 500 do
     $messages = stack(margin: 5, width: 445){ border gray } 
   end
   Thread.new do
-  	message = ''
-  	debug_message = ''
-  	loop do
-  		byte = serialport.read 1
-  		debug_message << byte
-			if byte == Port::END_SIGNAL
-				$debugger.clear do
+    message = ''
+    debug_message = ''
+    loop do
+      byte = serialport.read 1
+      debug_message << byte
+      if byte == Port::END_SIGNAL
+        $debugger.clear do
           border gray
-				  stack width: 240 do
-		        para fg debug_message, lightyellow
-		      end
-	      end 
-				$messages.append do
-      	  stack width: 200, margin_left: 250 do
-		        para fg message, lightyellow
-		      end
-	      end
-	      debug_message = ''
-				message = '' 
-			elsif byte == Port::JAM_SIGNAL
-				message.chop!	
-			else
-				message << byte	
-	    end
-	  end
-	end
+          stack width: 240 do
+            para fg debug_message, lightyellow
+          end
+        end 
+        $messages.append do
+          stack width: 200, margin_left: 250 do
+            para fg message, lightyellow
+          end
+        end
+        debug_message = ''
+        message = '' 
+      elsif byte == Port::JAM_SIGNAL
+        message.chop!  
+      else
+        message << byte  
+      end
+    end
+  end
 end
--- port.rb
+++ port.rb
@@ -2,31 +2,31 @@ require 'rubyserial'
 
 class Port < Serial
 
-	BAUD_ARRAY = [110, 150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
-	DELAY_SECONDS = 1
-	JAM_SIGNAL = '~'
-	END_SIGNAL = '}' 
-	attr_reader :adress
-	
-	def initialize adress ,baude_rate=9600, data_bits=8, parity=:none 
+  BAUD_ARRAY = [110, 150, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
+  DELAY_SECONDS = 1
+  JAM_SIGNAL = '~'
+  END_SIGNAL = '}' 
+  attr_reader :adress
+
+  def initialize adress ,baude_rate=9600, data_bits=8, parity=:none 
     @adress = adress
     super
   end
 
   def write data
-  	wait_chanel_free
-  	data.each_byte do |byte|
+    wait_chanel_free
+    data.each_byte do |byte|
       attemps = 0  
       loop do
-     	  super byte.chr
-  	    sleep 0.1
-  	    if collide?
-  	    	super JAM_SIGNAL 
-  	    	attemps += 1
-  	    	delay_write(attemps)
-  	    else
-  	    	break
-  	    end 
+        super byte.chr
+        sleep 0.1
+        if collide?
+          super JAM_SIGNAL 
+          attemps += 1
+          delay_write(attemps)
+        else
+          break
+        end 
       end
     end
     puts
@@ -39,20 +39,20 @@ class Port < Serial
    
   private
   def wait_chanel_free
-  	sleep(DELAY_SECONDS) if time_odd?
+    sleep(DELAY_SECONDS) if time_odd?
   end
 
   def time_odd?
-  	Time.now.to_i.odd?
+    Time.now.to_i.odd?
   end 
 
   def collide?
     flag = time_odd?
-  	print 'x' if flag
-  	flag
+    print 'x' if flag
+    flag
   end
 
   def delay_write attemp
-  	sleep(rand([10, attemp].min ** 2) / 100)
+    sleep(rand([10, attemp].min ** 2) / 100)
   end
 end
